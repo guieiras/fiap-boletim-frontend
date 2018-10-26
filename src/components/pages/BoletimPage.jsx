b314c923353b248b454d14d2bc3a9d09a3a2e514
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-10-26 14:47:42 -0300

    Making request to API

--- src/components/pages/BoletimPage.jsx
+++ src/components/pages/BoletimPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { Page, Navbar, BlockTitle } from 'framework7-react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Block, BlockTitle, Card, Navbar, Page } from 'framework7-react';
+import { decrypt } from '../../library/crypto';
 
-export default () => (
-  <Page>
-    <Navbar title="FIAP Boletim+"></Navbar>
-    <BlockTitle>Minhas Notas</BlockTitle>
-  </Page>
-);
+export default class BoletimPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: 'initial', boletim: null };
+  }
+
+  componentDidMount() {
+    const { rm, password } = decrypt(sessionStorage.getItem('_clientToken'));
+    axios
+      .post('http://localhost:4568/notas', { rm, senha: password })
+      .then((response) => {
+        this.setState({ boletim: response.data, status: 'success' });
+      });
+  }
+
+  render() {
+    return <Page>
+      <Navbar title="FIAP Boletim+"></Navbar>
+      <BlockTitle>Minhas Notas</BlockTitle>
+      { this.boletim() }
+    </Page>
+  }
+
+  boletim() {
+    switch(this.state.status) {
+      case 'initial':
+        return <Card content="Carregando Boletim" />
+      case 'success':
+        return <div>OK</div>
+    }
+  }
+}
